d66d188942d880215027731bebca96d556afb832
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 13:43:51 +0300

    added DefaultRpcFactory

--- obj/src/container/AccountsProcess.js
+++ obj/src/container/AccountsProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const AccountsServiceFactory_1 = require("../build/AccountsServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class AccountsProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("accounts", "User accounts microservice");
         this._factories.add(new AccountsServiceFactory_1.AccountsServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.AccountsProcess = AccountsProcess;
--- src/container/AccountsProcess.ts
+++ src/container/AccountsProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { AccountsServiceFactory } from '../build/AccountsServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class AccountsProcess extends ProcessContainer {
 
     public constructor() {
         super("accounts", "User accounts microservice");
         this._factories.add(new AccountsServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
