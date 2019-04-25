4c1a7b29a2897092ab5c38df6aef9dc4ffdb7794
Dmitriy Krayniy <krdima92@gmail.com>
2019-04-25 17:28:51 +0300

    couchbase persistence added to factory

--- src/build/AccountsServiceFactory.ts
+++ src/build/AccountsServiceFactory.ts
@@ -1,6 +1,7 @@
 import { Factory } from 'pip-services3-components-node';
 import { Descriptor } from 'pip-services3-commons-node';
 
+import { AccountsCouchbasePersistence } from '../persistence/AccountsCouchbasePersistence';
 import { AccountsMongoDbPersistence } from '../persistence/AccountsMongoDbPersistence';
 import { AccountsFilePersistence } from '../persistence/AccountsFilePersistence';
 import { AccountsMemoryPersistence } from '../persistence/AccountsMemoryPersistence';
@@ -14,6 +15,7 @@ export class AccountsServiceFactory extends Factory {
 	public static MemoryPersistenceDescriptor = new Descriptor("pip-services-accounts", "persistence", "memory", "*", "1.0");
 	public static FilePersistenceDescriptor = new Descriptor("pip-services-accounts", "persistence", "file", "*", "1.0");
 	public static MongoDbPersistenceDescriptor = new Descriptor("pip-services-accounts", "persistence", "mongodb", "*", "1.0");
+	public static CouchbasePersistenceDescriptor = new Descriptor("pip-services-accounts", "persistence", "couchbase", "*", "1.0");
 	public static ControllerDescriptor = new Descriptor("pip-services-accounts", "controller", "default", "*", "1.0");
 	public static HttpServiceDescriptor = new Descriptor("pip-services-accounts", "service", "http", "*", "1.0");
 	public static GrpcServiceDescriptor = new Descriptor("pip-services-accounts", "service", "grpc", "*", "1.0");
@@ -24,6 +26,7 @@ export class AccountsServiceFactory extends Factory {
 		this.registerAsType(AccountsServiceFactory.MemoryPersistenceDescriptor, AccountsMemoryPersistence);
 		this.registerAsType(AccountsServiceFactory.FilePersistenceDescriptor, AccountsFilePersistence);
 		this.registerAsType(AccountsServiceFactory.MongoDbPersistenceDescriptor, AccountsMongoDbPersistence);
+		this.registerAsType(AccountsServiceFactory.CouchbasePersistenceDescriptor, AccountsCouchbasePersistence);
 		this.registerAsType(AccountsServiceFactory.ControllerDescriptor, AccountsController);
 		this.registerAsType(AccountsServiceFactory.HttpServiceDescriptor, AccountsHttpServiceV1);
 		this.registerAsType(AccountsServiceFactory.GrpcServiceDescriptor, AccountsGrpcServiceV1);
