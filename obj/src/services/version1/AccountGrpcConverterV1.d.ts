d4c2581395eba4ffc810e4c2c31c4043ca6e3376
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2019-07-12 16:49:22 -0700

    Merged

--- obj/src/services/version1/AccountGrpcConverterV1.d.ts
+++ obj/src/services/version1/AccountGrpcConverterV1.d.ts
@@ -6,15 +6,10 @@ export declare class AccountGrpcConverterV1 {
     static toError(obj: any): any;
     static setMap(map: any, values: any): void;
     static getMap(map: any): any;
-<<<<<<< HEAD
     private static toJson;
     private static fromJson;
     static fromPagingParams(paging: PagingParams): any;
     static toPagingParams(obj: any): PagingParams;
-=======
-    private static toJson(value);
-    private static fromJson(value);
->>>>>>> 47b7a62324bf4a9b54a4f4ad95349fdf7296ea10
     static fromAccount(account: AccountV1): any;
     static toAccount(obj: any): AccountV1;
     static fromAccountPage(page: DataPage<AccountV1>): any;
